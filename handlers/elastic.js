463678fef768b48cfc45563215254ebc558fd2b3
Rhodri Karim <rhodri@gmail.com>
2015-07-08 20:12:12 +0100

    Split into separate event handlers.

--- handlers/elastic.js
+++ handlers/elastic.js
@@ -3,12 +3,12 @@ var esClient = require('../helpers/es-client'),
 
 var es = esClient(config.elastic);
 
-on('ElasticQuery', function (query) {
+on('ElasticOptions', function (query) {
+  return constructOptions(query);
+});
 
-  var searchOptions = constructOptions(query),
-      searchResultsPromise = es.search(searchOptions);
-
-  return searchResultsPromise.then(function (results) {
+on('ElasticQuery', function (searchOptions) {
+  return es.search(searchOptions).then(function (results) {
     return results.hits.hits.map(function (hit) { return hit._source; });
   });
 });
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var MicroService = require('persephone-ms');
 var ms = new MicroService();
 
-ms.query('ElasticQuery', { from : '2015-05-14Z', to: '2015-05-15Z' }).then(function (reply) {
-  ms.log.info("Reply received from Elastic", reply);
+ms.query('ElasticOptions', { from : '2015-05-14Z', to: '2015-05-15Z' }).then(function (searchOptions) {
+  return ms.query('ElasticQuery', searchOptions);
+}).then(function (results) {
+  ms.log.info("Reply received from Elastic", results);
 });
