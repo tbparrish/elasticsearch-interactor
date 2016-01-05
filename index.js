d996d4916f452cad1c8b5de12ea8687662e1296b
Matt Williams <matt@makeable.co.uk>
2016-01-05 13:11:54 +0000

    PER-998: stop outputting debug logs

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 var MicroService = require('persephone-ms');
 var ms = new MicroService();
+ms.log.transports.console.level = 'info'; // TODO: Move this into a config option handled by the ms framework.
