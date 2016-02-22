e183c3686028d1a747eee9bc723bc4d5ae5e7a85
Terry Parrish <terry.parrish@guardtime.com>
2016-02-22 15:34:03 -0500

    PER-1051 - Performance dashboard -- Change network stats to rate based instead of actual count

--- helpers/metrics/interfacesPackets.js
+++ helpers/metrics/interfacesPackets.js
@@ -1,6 +1,7 @@
 var hasHostsBuckets = require("../utils").hasHostsBuckets,
     mq = require("../metrics-query"),
-    moment = require('moment');
+    moment = require('moment'),
+    transformInterface = require("./transformInterface");
 
 function multiLineChart(_aggs) {
   function aggregation (from, to, interval) {
@@ -25,44 +26,14 @@ function multiLineChart(_aggs) {
     };
   }
 
-  // TODO: look into calcaluting using script
   function transform(results) {
-    var key = "", idx = 0, key_as_string = "", i = 0, xValues = [], txValues = [], retVal = [];
-    if (!hasHostsBuckets(results))
+    if (!hasHostsBuckets(results)) {
       return [];
-    var hosts = results.aggregations.hosts.buckets;
-
-    for (i = 0; i < hosts.length; i += 1) {
-        key = hosts[i].key;
-        rxValues = [];
-        txValues = [];
-
-        for (var j = 0;  j < hosts[i].time.buckets.length - 1; j += 1) {
-          idx = j + 1;
-
-          key_as_string = hosts[i].time.buckets[idx].key_as_string;
-
-          if((hosts[i].time.buckets[idx].rx.value !== null) && (hosts[i].time.buckets[j].rx.value !== null)) {
-            if((hosts[i].time.buckets[idx].rx.value - hosts[i].time.buckets[j].rx.value) >= 0) {
-              rxValues.push({x: key_as_string,
-                y: (hosts[i].time.buckets[idx].rx.value - hosts[i].time.buckets[j].rx.value)});
-            }
-          }
-
-          if((hosts[i].time.buckets[idx].tx.value !== null) && (hosts[i].time.buckets[j].tx.value !== null)) {
-            if((hosts[i].time.buckets[idx].tx.value - hosts[i].time.buckets[j].tx.value) >= 0) {
-              txValues.push({x: key_as_string,
-                y: (hosts[i].time.buckets[idx].tx.value - hosts[i].time.buckets[j].tx.value)});
-            }
-          }
-
-        }
-        retVal.push({ key: key+" rx", values: rxValues});
-        retVal.push({ key: key+" tx", values: txValues});
-      }
-      return retVal;
+    } else {
+      return transformInterface.transform(results);
     }
-    return { aggregation: aggregation, transform: transform };
+  }
+  return { aggregation: aggregation, transform: transform };
 }
 
 function aggregation() {
---
+++ helpers/metrics/transformInterface.js
@@ -0,0 +1,105 @@
+var moment = require('moment');
+
+// TODO - make promises to calculate rx and tx asynchronously
+var transform = function(results) {
+  var key = "", startingIdx = 0, startTime, endTime, endingIdx = 1, seconds = 0, key_as_string = "", i = 0, xValues = [], txValues = [], retVal = [];
+  var hosts = results.aggregations.hosts.buckets;
+
+  endingIdx = 1;
+  for (i = 0; i < hosts.length; i += 1) {
+      key = hosts[i].key;
+      rxValues = [];
+
+      endingIdx = 1;
+      for (startingIdx = 0;  (startingIdx < hosts[i].time.buckets.length - 1) && (endingIdx < hosts[i].time.buckets.length); startingIdx += 1) {
+
+        // find starting index
+        while((startingIdx < hosts[i].time.buckets.length - 1) && (endingIdx < hosts[i].time.buckets.length) &&
+              ((typeof hosts[i].time.buckets[startingIdx].rx.value === "undefined") || (hosts[i].time.buckets[startingIdx].rx.value === null) ||
+              (typeof hosts[i].time.buckets[startingIdx].key_as_string === "undefined") || (hosts[i].time.buckets[startingIdx].key_as_string === null))) {
+                startingIdx += 1;
+                endingIdx = startingIdx+1;
+        }
+
+        // find ending index
+        while( (endingIdx < hosts[i].time.buckets.length) &&
+              ((typeof hosts[i].time.buckets[endingIdx].rx.value === "undefined") || (hosts[i].time.buckets[endingIdx].rx.value === null) ||
+              (typeof hosts[i].time.buckets[endingIdx].key_as_string === "undefined") || (hosts[i].time.buckets[endingIdx].key_as_string === null))) {
+            endingIdx += 1;
+        }
+
+        // make sure we have a positive value
+        while((endingIdx < hosts[i].time.buckets.length) &&
+              ((hosts[i].time.buckets[endingIdx].rx.value - hosts[i].time.buckets[startingIdx].rx.value) < 0)) {
+            endingIdx += 1;
+        }
+
+        if((startingIdx < hosts[i].time.buckets.length - 1) && (endingIdx < hosts[i].time.buckets.length) &&
+          (hosts[i].time.buckets[endingIdx].rx.value - hosts[i].time.buckets[startingIdx].rx.value) >= 0) {
+          endTime = moment(moment(hosts[i].time.buckets[endingIdx].key_as_string).toArray());
+          startTime = moment(moment(hosts[i].time.buckets[startingIdx].key_as_string).toArray());
+          seconds = endTime.diff(startTime, 'seconds');
+
+          rxValues.push({x: hosts[i].time.buckets[endingIdx].key_as_string,
+            y: (hosts[i].time.buckets[endingIdx].rx.value - hosts[i].time.buckets[startingIdx].rx.value)/seconds});
+        }
+
+        startingIdx = endingIdx-2;
+        endingIdx += 1;
+      }
+
+      retVal.push({ key: key+" rx", values: rxValues});
+    }
+
+    endingIdx = 1;
+    for (i = 0; i < hosts.length; i += 1) {
+        key = hosts[i].key;
+        txValues = [];
+
+        endingIdx = 1;
+        for (startingIdx = 0;  (startingIdx < hosts[i].time.buckets.length - 1) && (endingIdx < hosts[i].time.buckets.length); startingIdx += 1) {
+
+          // find starting index
+          while((startingIdx < hosts[i].time.buckets.length - 1) && (endingIdx < hosts[i].time.buckets.length) &&
+                ((typeof hosts[i].time.buckets[startingIdx].tx.value === "undefined") || (hosts[i].time.buckets[startingIdx].tx.value === null) ||
+                (typeof hosts[i].time.buckets[startingIdx].key_as_string === "undefined") || (hosts[i].time.buckets[startingIdx].key_as_string === null))) {
+                  startingIdx += 1;
+                  endingIdx = startingIdx+1;
+          }
+
+          // find ending index
+          while( (endingIdx < hosts[i].time.buckets.length) &&
+                ((typeof hosts[i].time.buckets[endingIdx].tx.value === "undefined") || (hosts[i].time.buckets[endingIdx].tx.value === null) ||
+                (typeof hosts[i].time.buckets[endingIdx].key_as_string === "undefined") || (hosts[i].time.buckets[endingIdx].key_as_string === null))) {
+              endingIdx += 1;
+          }
+
+          // make sure we have a positive value
+          while((endingIdx < hosts[i].time.buckets.length) &&
+                ((hosts[i].time.buckets[endingIdx].tx.value - hosts[i].time.buckets[startingIdx].tx.value) < 0)) {
+              endingIdx += 1;
+          }
+
+          if((startingIdx < hosts[i].time.buckets.length - 1) && (endingIdx < hosts[i].time.buckets.length) &&
+            (hosts[i].time.buckets[endingIdx].tx.value - hosts[i].time.buckets[startingIdx].tx.value) >= 0) {
+            endTime = moment(moment(hosts[i].time.buckets[endingIdx].key_as_string).toArray());
+            startTime = moment(moment(hosts[i].time.buckets[startingIdx].key_as_string).toArray());
+            seconds = endTime.diff(startTime, 'seconds');
+
+            txValues.push({x: hosts[i].time.buckets[endingIdx].key_as_string,
+              y: (hosts[i].time.buckets[endingIdx].tx.value - hosts[i].time.buckets[startingIdx].tx.value)/seconds});
+          }
+
+          startingIdx = endingIdx-2;
+          endingIdx += 1;
+        }
+
+        retVal.push({ key: key+" tx", values: txValues});
+      }
+
+    return retVal;
+};
+
+module.exports = {
+    transform: transform
+};
